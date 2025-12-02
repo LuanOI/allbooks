30f4918508409a535e42e9a267cfea359f28080c
Luan Oliveira <lssob@LuanAcerNitro>
2025-12-02 19:52:41 -0300

    altera

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -19,6 +19,6 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log('Boas-Vindas')
-  console.log("API indisponível em http://localhost:8000")
+  console.log("API disponível em http://localhost:8000")
 })
                 
